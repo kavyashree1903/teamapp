6770e74a31f26ec760979803e4c16cfb5bfea9dd
kavya <kavya.shree@venuemonk.com>
2020-06-14 13:33:58 +0530

    Updates

---
+++ precache-manifest.0a94f2cba59431d2af560c8ffbf4755f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61e81a47e1fb65243f339923a1b3912",
+    "url": "/team-app/index.html"
+  },
+  {
+    "revision": "928aae5f1bddec7222fb",
+    "url": "/team-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c9354985061556284af2",
+    "url": "/team-app/static/js/2.7a7259fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/team-app/static/js/2.7a7259fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928aae5f1bddec7222fb",
+    "url": "/team-app/static/js/main.afbc5128.chunk.js"
+  },
+  {
+    "revision": "ae548c0793ae441b6ee8",
+    "url": "/team-app/static/js/runtime-main.62fff233.js"
+  }
+]);
