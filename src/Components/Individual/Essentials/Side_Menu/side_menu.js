8f6c443ddfca3aef28cb9e3bb91467b43038cd30
devaansh100 <devaanshgupta100@gmail.com>
2020-06-19 01:19:13 +0530

    Add side menu component

---
+++ src/Components/Individual/Essentials/Side_Menu/side_menu.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+
+
+
+class SideMenu extends Component{
+	constructor(){
+		state = {
+
+		}
+		var buttonList = this.props.buttons.map((button) => { //this.props.buttons will each have unique onclick listeners
+			return (
+					<li class="side-menu-button">
+						<Button 
+							text={button.name} 
+							onClick={button.clicked}
+						/>
+						
+					</li>
+				)
+		})
+	}
+
+	render(){
+		return{
+			<div class="side-menu">
+				{buttonList}
+			</div>
+		}
+	}
+}
+
+export default SideMenu
