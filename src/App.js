26e468179a3a2940abc5fb0eb0569e62c6ad8cc8
devaansh100 <devaanshgupta100@gmail.com>
2020-06-19 04:09:06 +0530

    Background color added. Code for including navbar is added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import SideMenu from './Components/Individual/Essentials/Side_Menu/side_menu.js'
+import Navbar from './Components/Individual/Essentials/Navbar/navbar.js'
 
 class App extends Component{
 	constructor(){
@@ -13,33 +14,19 @@ class App extends Component{
 			{
 				name: "Button 2",
 				clicked: () => console.log("Button 2 pressed") //will eventually change to interact with database
-			},
-			{
-				name: "Button 1",
-				clicked: () => console.log("Button 1 pressed") //will eventually change to interact with database
-			},
-			{
-				name: "Button 1",
-				clicked: () => console.log("Button 1 pressed") //will eventually change to interact with database
-			},
-			{
-				name: "Button 1",
-				clicked: () => console.log("Button 1 pressed") //will eventually change to interact with database
-			},
-			{
-				name: "Button 1",
-				clicked: () => console.log("Button 1 pressed") //will eventually change to interact with database
-			},
-			{
-				name: "Button 1",
-				clicked: () => console.log("Button 1 pressed") //will eventually change to interact with database
 			}
 		]
+		//write code for left and right side menu on-click listeners
+		//write code for the slide in slide out animation
+		//write code for blurring background when sied menu is shown
 	}
 
 	render(){
 		return(
-			<SideMenu buttons={this.sideMenuButtons} />
+			<div className="container-app">
+				<Navbar hamburger-left={this.toggleLeftSideMenu} hamburger-right={this.toggleRightSideMenu} />
+				<SideMenu buttons={this.sideMenuButtons} />
+			</div>
 		)
 	}
 }
