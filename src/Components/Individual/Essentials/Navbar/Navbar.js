65affaf1d1436fdf10db30480e147c0d1864afbe
devaansh100 <devaanshgupta100@gmail.com>
2020-06-21 02:04:55 +0530

    Made navbar work with side menu

--- src/Components/Individual/Essentials/Navbar/Navbar.js
+++ src/Components/Individual/Essentials/Navbar/Navbar.js
@@ -5,13 +5,13 @@ class Navbar extends React.Component{
 	render() {
 		return (
 			<div>
-				<ul id="nav">
+				<div id="nav">
 					 <div className="container-navbar">
 					 <img className="left-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
 					 <a className="app-title" href="#">WORC</a>
 					 <img className="right-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
 					 </div>
-				</ul>
+				</div>
 			</div>
 		)
 	}
