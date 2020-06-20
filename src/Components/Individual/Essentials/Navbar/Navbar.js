01fa17ad4703a0b72186f2d745745c843c4af589
devaansh100 <devaanshgupta100@gmail.com>
2020-06-21 01:27:11 +0530

    fix indentation

--- src/Components/Individual/Essentials/Navbar/Navbar.js
+++ src/Components/Individual/Essentials/Navbar/Navbar.js
@@ -4,15 +4,15 @@ import "./Navbar.css";
 class Navbar extends React.Component{
 	render() {
 		return (
-				<div>
-					<ul id="nav">
-					 <div className="container-navbar">
-					 <img className="left-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
-					 <a className="app-title" href="#">WORC</a>
-					 <img className="right-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
-					 </div>
-					</ul>
-				</div>
+			<div>
+				<ul id="nav">
+				 <div className="container-navbar">
+				 <img className="left-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
+				 <a className="app-title" href="#">WORC</a>
+				 <img className="right-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
+				 </div>
+				</ul>
+			</div>
 		)
 	}
 }
