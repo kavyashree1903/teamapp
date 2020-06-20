4cb9ca73541524509a3abd6a7971443732a753c4
devaansh100 <devaanshgupta100@gmail.com>
2020-06-21 01:37:35 +0530

    Bug fixes

--- src/Components/Individual/Essentials/Navbar/Navbar.js
+++ src/Components/Individual/Essentials/Navbar/Navbar.js
@@ -6,11 +6,11 @@ class Navbar extends React.Component{
 		return (
 			<div>
 				<ul id="nav">
-				 <div className="container-navbar">
-				 <img className="left-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
-				 <a className="app-title" href="#">WORC</a>
-				 <img className="right-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
-				 </div>
+					 <div className="container-navbar">
+					 <img className="left-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
+					 <a className="app-title" href="#">WORC</a>
+					 <img className="right-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg"/>
+					 </div>
 				</ul>
 			</div>
 		)
