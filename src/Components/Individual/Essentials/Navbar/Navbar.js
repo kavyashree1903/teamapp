a0c4378e8d4be4f5946e5c27a31f20c426608c66
devaansh100 <devaanshgupta100@gmail.com>
2020-06-21 18:41:21 +0530

    Connect side bar with hamburger icons

--- src/Components/Individual/Essentials/Navbar/Navbar.js
+++ src/Components/Individual/Essentials/Navbar/Navbar.js
@@ -1,36 +1,48 @@
 import React from 'react';
 import {Component} from 'react';
-import "./Navbar.css";
+import './Navbar.css';
+import SideMenu from '../Side_Menu/side_menu.js'
 
-var sideMenuClasses = "";
-var currentState;
-class Navbar extends React.Component{
+
+var sideMenuClasses = 'container-side-menu hide';
+class Navbar extends Component{
 	constructor(props){
 		super(props)
-		this.sideMenuClasses = "container-side-menu hide"
-		this.offState ={
+		this.offState = {
 			right: false,
 			left: false
 		}
-		this.leftState ={
+		this.leftState = {
 			right: false,
 			left: true
 		}
-		this.rightState ={
+		this.rightState = {
 			right: true,
 			left: false
 		}
-		this.state = this.offState
+		this.state = {
+			...this.offState
+		}
+		this.sideMenuButtons = [
+			{
+				name: "Button 1",
+				clicked: () => console.log("Button 1 pressed") //will eventually change to interact with database
+			},
+			{
+				name: "Button 2",
+				clicked: () => console.log("Button 2 pressed") //will eventually change to interact with database
+			}
+		]
 	}
 	leftSideMenuClick(currentState){
-		if(currentState.left == true && currentState.right == false){
+		if(currentState.left === true && currentState.right === false){
 			this.setState(
 				{
 					right: false,
 					left: false
 				}
 			)
-			this.sideMenuClasses = "container-side-menu hide"//BT
+			sideMenuClasses = "container-side-menu hide"
 		}
 		else{
 			this.setState(
@@ -39,19 +51,19 @@ class Navbar extends React.Component{
 					left: true
 				}
 			)
-			this.sideMenuClasses = "container-side-menu left"//BT
+			sideMenuClasses = "container-side-menu left"
 		}
 		
 	}
 	rightSideMenuClick(currentState){
-		if(currentState.left == false && currentState.right == true){
+		if(currentState.left === false && currentState.right === true){
 			this.setState(
 				{
 					right: false,
 					left: false
 				}
 			)
-			this.sideMenuClasses = "container-side-menu hide"//BT
+			sideMenuClasses = "container-side-menu hide"
 		}
 		else{
 			this.setState(
@@ -60,23 +72,25 @@ class Navbar extends React.Component{
 					left: false
 				}
 			)
-			this.sideMenuClasses = "container-side-menu right"//BT
+			sideMenuClasses = "container-side-menu right"
 		}
 		
 	}
 	render() {
-		sideMenuClasses = this.sideMenuClasses
-		currentState = this.state
+		var sideMenu = <SideMenu stateClasses={sideMenuClasses} buttons={[]}/>
 		return (
-			<div>
-				<div id="nav">
-					 <div className="container-navbar">
-					 <img className="left-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg" onClick = {(currentState) => this.leftSideMenuClick(currentState)}/>
-					 <span><a className="app-title" href="#">WORC</a></span>
-					 <img className="right-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg" onClick = {(currentState) => this.rightSideMenuClick(currentState)}/>
-					 </div>
+			<React.Fragment>
+				<div>
+					<div id="nav">
+						 <div className="container-navbar">
+						 <img className="left-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg" onClick = {() => this.leftSideMenuClick(this.state)}/>
+						 <span><a className="app-title" href="#">WORC</a></span>
+						 <img className="right-hamburger" src="https://ik.imagekit.io/m52sq26n4h/menu-white-18dp.svg" onClick = {() => this.rightSideMenuClick(this.state)}/>
+						 </div>
+					</div>
 				</div>
-			</div>
+				 <SideMenu stateClasses={sideMenuClasses} buttons={this.sideMenuButtons}/>
+			 </React.Fragment>
 		)
 	}
 }
--- src/Components/Individual/Essentials/Side_Menu/side_menu.js
+++ src/Components/Individual/Essentials/Side_Menu/side_menu.js
@@ -1,15 +1,10 @@
 import React, {Component} from 'react'
 import SideMenuButton from './side_menu_button/side_menu_button.js'
 import './side_menu.css'
-import {sideMenuClasses} from '../Navbar/Navbar.js'
 
 class SideMenu extends Component{
 	constructor(props){
 		super(props)
-		this.state = {
-			left: false,
-			right: false
-		}
 		this.buttonList = this.props.buttons.map((button) => { //this.props.buttons will each have unique onclick listeners
 			return (
 				<div>
@@ -24,7 +19,7 @@ class SideMenu extends Component{
 
 	render(){
 		return(
-			<div className={sideMenuClasses}>
+			<div className={this.props.stateClasses}>
 				<div className="side-menu">
 					{this.buttonList}
 				</div>
