6c067da515282016da2fa9bec1b4a1479ad05dea
devaansh100 <devaanshgupta100@gmail.com>
2020-07-04 02:02:54 +0530

    Add basic javascript for the carousel

--- src/App.js
+++ src/App.js
@@ -9,23 +9,82 @@ import CalendarDashboard from "./Components/Collective/Meeting_Dashboard/Meeting
 class App extends Component {
   constructor() {
     super();
-    this.dashboardInView = <MeetingDashboard />
+    this.state = {
+      currentView: 1,
+      leftView: 3, //set to number of views
+      rightView: 2
+    }
+    this.numberOfViews = 3
+    this.views = 
+    [
+      {
+        id: 1,
+        view: <MeetingDashboard />,
+        left: this.numberOfViews,
+        right: 2
+      },
+      {
+        id: 2,
+        view: <TaskDashboard />,
+        left: 1,
+        right: 3
+      },
+      {
+        id: 3,
+        view: <CalendarDashboard />,
+        left: 2,
+        right: 1
+      }
+    ]    
   }
 
-  render() {
+  showLeftView(currentState){
+    var newLeftView
+    if(currentState.leftView === 1) 
+      newLeftView = this.numberOfViews //to make it like a circle
+    else
+      newLeftView = currentState.leftView - 1
 
-    this.dashboardInView = <MeetingDashboard />
+    var newState = {
+      currentView: currentState.leftView,
+      leftView: this.views[newLeftView - 1].id, //-1 to account for the array indices starting from 0
+      rightView: currentState.currentView
+    }
+    document.querySelector('.left-dashboard').classList.add('roll-to-current-from-left')    
+    document.querySelector('.current-dashboard').classList.add('roll-to-right-from-current')    
+    document.querySelector('.right-dashboard').classList.add('roll-to-left-from-right')    
+    this.setState(newState)
+  }
+
+  showRightView(currentState){
+    var newRightView
+    if(currentState.rightView === this.numberOfViews) 
+      newRightView = 1 //to make it like a circle
+    else
+      newRightView = currentState.rightView + 1
+
+    var newState = {
+      currentView: currentState.rightView,
+      rightView: this.views[newRightView - 1].id, //-1 to account for the array indices starting from 0
+      leftView: currentState.currentView
+    }
+    document.querySelector('.right-dashboard').classList.add('roll-to-current-from-right')    
+    document.querySelector('.current-dashboard').classList.add('roll-to-left-from-current')    
+    document.querySelector('.left-dashboard').classList.add('roll-to-right-from-left')
+    this.setState(newState)
+    
+  }
+
+
+  render() {
     return (
       <React.Fragment>
-        <Navbar
-          hamburger-left={this.toggleLeftSideMenu}
-          hamburger-right={this.toggleRightSideMenu}
-        />
+        <Navbar />
         <div className="container-app">
           <div className="container-app-dashboard">
-            <div className="left-dashboard"></div>
-            <div className="current-dashboard">{this.dashboardInView}</div>
-            <div className="right-dashboard"></div>
+            <div onClick= {() => this.showLeftView(this.state)} className="left-dashboard">{this.views[this.state.leftView - 1].view}</div>
+            <div className="current-dashboard">{this.views[this.state.currentView - 1].view}</div>
+            <div onClick= {() => this.showRightView(this.state)} className="right-dashboard">{this.views[this.state.rightView - 1].view}</div>
           </div>
         </div>
       </React.Fragment>
