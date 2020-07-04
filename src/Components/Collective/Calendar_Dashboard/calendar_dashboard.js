aea842ca1a489755c7e090f7079a4f363da23fff
devaansh100 <devaanshgupta100@gmail.com>
2020-07-05 02:07:16 +0530

    Add calendar view buttons

--- src/Components/Collective/Calendar_Dashboard/calendar_dashboard.js
+++ src/Components/Collective/Calendar_Dashboard/calendar_dashboard.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react'
+import './calendar_dashboard.css'
 
 class CalendarDashboard extends Component{
 	render(){
 		return(
-	 <div className="outer-container-dashboard">
-            <div className="background-for-dashboard">
-                <h1> calendar </h1>
-            </div>
-        </div>)
+			<div className="outer-container-dashboard">
+	            <div className="background-for-dashboard">
+	            	<div className="inner-container-for-calendar">
+	            		<div onClick={this.expandDay}className="day-block">
+	            			<p className="day-text" >Sunday</p>
+	            		</div>
+	            		<div onClick={this.expandDay}className="day-block">
+	            			<p className="day-text" >Monday</p>
+	            		</div>
+	            		<div onClick={this.expandDay}className="day-block">
+	            			<p className="day-text" >Tuesday</p>
+	            		</div>
+	            		<div onClick={this.expandDay}className="day-block">
+	            			<p className="day-text" >Wednesday</p>
+	            		</div>
+	            		<div onClick={this.expandDay}className="day-block">
+	            			<p className="day-text" >Thursday</p>
+	            		</div>
+	            		<div onClick={this.expandDay}className="day-block">
+	            			<p className="day-text" >Friday</p>
+	            		</div>
+	            		<div onClick={this.expandDay}className="day-block">
+	            			<p className="day-text" >Saturday</p>
+	            		</div>
+		            </div>    
+	            </div>
+	        </div>
+        )
 
 	}
 }
