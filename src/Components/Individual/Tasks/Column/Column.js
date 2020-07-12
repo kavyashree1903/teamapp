601c42587194c7e4b62a1af01d7b4ea9ddd1238b
devaansh100 <devaanshgupta100@gmail.com>
2020-07-12 18:23:05 +0530

    Fix Formatting

--- src/Components/Individual/Tasks/Column/Column.js
+++ src/Components/Individual/Tasks/Column/Column.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./Column.css";
 class Column extends Component {
-  render() {
-    return (
-      <div className="task-column">
-        <div className="column-heading">
-          <p className="column-status">{this.props.heading}</p>
-        </div>
-      </div>
+
+render() {
+	return (
+        <div className="task-column">
+        	<div className="column-heading">
+        		<p className="column-status">{this.props.heading}</p>
+        	</div>
+      	</div>
     );
   }
 }
